73edb700735356a2a4da0caeb384bb79473187a4
Faiva Puspa Sahara <puspafai5@gmail.com>
2026-02-23 23:47:13 +0700

    praktikum2 (langkah 8-11)

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 
-Route::get('/', HomeController::class);
-Route::get('/about', AboutController::class);
-Route::get('/articles/{id}', ArticleController::class);
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
+
 
 
